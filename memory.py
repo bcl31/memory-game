8f4a432a4873b34cc28413ba838135911ae2136f
bcl31 <17791229+bcl31@users.noreply.github.com>
2020-11-13 02:12:31 -0700

    Add files via upload

--- memory.py
+++ memory.py
@@ -1,3 +1,7 @@
+# this program is a simple matching game, in the top left a timer increases until all tiles have been matched.
+# if an incorrect match was made, the game will freeze for a one second penalty before hiding the tiles again
+# tiles are selected by clicking on them, at which point they will be revealed
+
 import pygame
 import random
 import time
@@ -23,7 +27,7 @@ def main():
 
 
 # User-defined classes
-
+# tracks general gameplay specific features
 class Game:
     # An object in this class represents a complete game.
 
@@ -42,6 +46,9 @@ class Game:
         self.continue_game = True
 
         # === objects that are only a part of memory
+
+        # --- Clock
+        self.clock = clock_render()
 
         # --- Tiles
 
@@ -65,6 +72,7 @@ class Game:
 
         for x in range(tiles_per_row):
             for y in range(tiles_per_column):
+
                 # find the location of the tile by multiplying the tile spacing by the grid coordinate value for
                 # the respective axis by it's tile spacing, then adding the space_between to prevent tiles touching
                 tile_location = [spacing + space_between_tiles[0] * x, spacing + space_between_tiles[1] * y]
@@ -75,6 +83,7 @@ class Game:
         # create an attribute to track tiles currently 'active' (not covered and not matched)
         self.active_tiles = []
 
+    # this method handles the loop of all processes that occur every frame
     def play(self):
         # Play the game until the player presses the close box.
         # - self is the Game that should be continued or not.
@@ -83,39 +92,53 @@ class Game:
             # play frame
             self.handle_events()
             self.draw()
+
+            # if all tiles have not yet been revealed
             if self.continue_game:
                 self.update()
                 self.decide_continue()
             self.game_Clock.tick(self.FPS)  # run at most with FPS Frames Per Second
 
+    # this method handles all action related to user input
     def handle_events(self):
-        # Handle each user event by changing the game state appropriately.
-        # - self is the Game whose events will be handled
-
+        # get all inputs that have occurred since last check
         events = pygame.event.get()
         for event in events:
+
+            # if the event was an attempt to close the game, flag the game to be closed
             if event.type == pygame.QUIT:
                 self.close_clicked = True
+
+            # if the event was a mouse click, check if a tile was clicked on and which one
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.click_check()
 
+    # this method is responsible for drawing all graphical elements to the screen
     def draw(self):
-        # Draw all game objects.
 
-        # - self is the Game to draw
         self.surface.fill(self.bg_color)  # clear the display surface first
 
         # draw tiles to screen
         for tile in self.deck:
             tile.draw()
 
+        # draw clock to screen
+        self.surface.blit(self.clock, (self.surface.get_size()[0] - self.clock.get_size()[0], 0))
+
         pygame.display.update()  # make the updated surface appear on the display
 
+    # this method is responsible for updating all game objects that are not influenced directly by the player
     def update(self):
-        # Update the game objects for the next frame.
+
+        # check if active cards are matching
         self.match_check()
 
+        # update the clock
+        self.clock = clock_render()
+
+    # this method checks if the active cards are matching, if not, it waits one second before hiding them again
     def match_check(self):
+        # check if more than one card is active, if so, check if they match
         if len(self.active_tiles) > 1:
             tile1, tile2 = self.active_tiles
             pair = tile1.pair_check(tile2)
@@ -129,29 +152,46 @@ class Game:
             # clear the tiles from the active tile list regardless
             self.active_tiles = []
 
+    # this method is responsible for checking if the game has come to an end, if so, it marks continue_game as False
     def decide_continue(self):
-        # Check and remember if the game should continue
-        # - self is the Game to check
+
+        # check if any tiles are still hidden
         uncovered = []
         for tile in self.deck:
             if tile.check_hidden():
                 uncovered.append(tile)
+
+        # if no tiles are still hidden, all tiles must be matched, therefore the game is over
         if len(uncovered) <= 0:
             self.continue_game = False
 
+    # this method should only be called when a mouse down event is detected. when called, it checks if the mouse
+    # position is currently over any of the tiles, if so, it flips the tile and adds it to the active tile list
     def click_check(self):
+        # check if mouse is colliding with any hidden tiles
         mouse_position = pygame.mouse.get_pos()
         for tile in self.deck:
             if tile.collision_check(mouse_position) and tile.check_hidden():
+                # if so, flip tile and mark it as active
                 tile.flip()
                 self.active_tiles.append(tile)
 
 
+# this class represents one tile in the memory game
+# a tile has a front and a back side
+# a tile can either be face up or face down
+# a tile displays the side that is currently up
+# a tile is a rectangle of a specified size
+# a tile is located at a specific location on the screen
 class Tile:
 
+    # dimensions is a list containing the dimensions of the tile in [width, height] format
+    # location is a list containing the coordinates for the top left corner of the tile in [x, y] format
+    # image is a string containing the name of the image file for the front side of the tile
+    # hidden_image is a string containing the name of the image file for the back side of the tile
+    # screen is the surface the tile will be drawn to
     def __init__(self, dimensions, location, image, hidden_image, screen):
         self.rect = pygame.Rect(location, dimensions)
-        self.color = pygame.Color('white')
         self.screen = screen
         self.hidden = True
         self.front_id = image
@@ -162,11 +202,15 @@ class Tile:
         back = pygame.image.load(hidden_image)
         self.back = pygame.transform.scale(back, dimensions)
 
+    # this method is responsible for drawing the tile to the screen
     def draw(self):
+        # if the tile is hidden (face-down) set the image to the backside, otherwise set the image to the front side
         if self.hidden:
             image = self.back
         else:  # if not self.hidden
             image = self.front
+
+        # print the decided image to the screen at the location of the tile's rect
         self.screen.blit(image, self.rect)
 
     # pos is a list or tuple containing coordinates
@@ -214,7 +258,7 @@ def image_list_shuffled():
 # tiles_per_row is an int containing the number of tiles that have to fit horizontally across the screen
 # tiles_per_column is an int containing the number of tiles that have to fit vertically stacked on the screen
 # space between is an int containing the size of the border between cards, in pixels
-# screen is the surface the cards will later be printed to
+# screen is the surface the tile will later be printed to
 
 # this function finds the necessary spacing between tile items coordinates in order to fit properly within the window
 # it returns a list containing the necessary spacing in [x, y] format
@@ -226,6 +270,22 @@ def find_tile_spacing(tiles_per_row, tiles_per_column, space_between, screen):
     tile_spacing[0] = dimensions[0] // tiles_per_row
     tile_spacing[1] = dimensions[1] // tiles_per_column
     return tile_spacing
+
+
+# this method creates a surface containing a number indicating the amount of seconds since the game started
+def clock_render():
+    # Text settings
+    font_size = 30
+    font = pygame.font.SysFont("arial", font_size)
+    color = pygame.Color("white")
+
+    # render the clock
+    game_time = pygame.time.get_ticks()
+    game_time = game_time / 1000
+    game_time = int(round(game_time, 0))  # round to whole number
+    game_time = str(game_time)
+    clock = pygame.font.Font.render(font, game_time, True, color)
+    return clock
 
 
 main()
