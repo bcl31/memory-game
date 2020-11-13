2e5684ec29e1c08b7b15f43a556114f2e082a986
bcl31 <17791229+bcl31@users.noreply.github.com>
2020-11-12 18:58:15 -0700

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,80 @@
+import pygame
+
+
+# User-defined functions
+
+def main():
+    # initialize all pygame modules (some need initialization)
+    pygame.init()
+    # create a pygame display window
+    pygame.display.set_mode((500, 400))
+    # set the title of the display window
+    pygame.display.set_caption('Memory')
+    # get the display surface
+    w_surface = pygame.display.get_surface()
+    # create a game object
+    game = Game(w_surface)
+    # start the main game loop by calling the play method on the game object
+    game.play()
+    # quit pygame and clean up the pygame window
+    pygame.quit()
+
+
+# User-defined classes
+
+class Game:
+    # An object in this class represents a complete game.
+
+    def __init__(self, surface):
+        # Initialize a Game.
+        # - self is the Game to initialize
+        # - surface is the display window surface object
+
+        # === objects that are part of every game that we will discuss
+        self.surface = surface
+        self.bg_color = pygame.Color('black')
+
+        self.FPS = 60
+        self.game_Clock = pygame.time.Clock()
+        self.close_clicked = False
+        self.continue_game = True
+
+    def play(self):
+        # Play the game until the player presses the close box.
+        # - self is the Game that should be continued or not.
+
+        while not self.close_clicked:  # until player clicks close box
+            # play frame
+            self.handle_events()
+            self.draw()
+            self.update()
+            self.game_Clock.tick(self.FPS)  # run at most with FPS Frames Per Second
+
+    def handle_events(self):
+        # Handle each user event by changing the game state appropriately.
+        # - self is the Game whose events will be handled
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.close_clicked = True
+
+    def draw(self):
+        # Draw all game objects.
+        # - self is the Game to draw
+        self.surface.fill(self.bg_color)  # clear the display surface first
+        pygame.display.update()  # make the updated surface appear on the display
+
+    def update(self):
+        # Update the game objects for the next frame.
+        # - self is the Game to update
+        pass
+
+
+class Card:
+
+    def __init__(self):
+        pass
+
+
+main()
