3488fc23298c045c3f8936c759e5af9f5feb5e62
bcl31 <17791229+bcl31@users.noreply.github.com>
2020-11-12 19:54:51 -0700

    Update memory.py

--- memory.py
+++ memory.py
@@ -7,7 +7,7 @@ def main():
     # initialize all pygame modules (some need initialization)
     pygame.init()
     # create a pygame display window
-    pygame.display.set_mode((500, 400))
+    pygame.display.set_mode((1000, 900))
     # set the title of the display window
     pygame.display.set_caption('Memory')
     # get the display surface
@@ -63,9 +63,12 @@ class Game:
 
     def draw(self):
         # Draw all game objects.
-        self.deck.draw()
+
         # - self is the Game to draw
         self.surface.fill(self.bg_color)  # clear the display surface first
+
+        self.deck.draw()
+
         pygame.display.update()  # make the updated surface appear on the display
 
     def update(self):
@@ -77,18 +80,29 @@ class Game:
 class Deck:
 
     def __init__(self, cards_per_row, cards_per_column, space_between, screen):
+
+        # find the necessary spacing and size of the cards relative to window size, this allows window size and
+        # number of cards to change dynamically with few issues.
+        cards_per_row += 1  # reserves a column of equal width to the rest for the score
         dimensions = list(screen.get_size())
-        dimensions[0] = dimensions[0] - 100  # this allows space for score on right
+        dimensions[1] = dimensions[1] - space_between  # this allows for spacing on bottom
         self.card_spacing_x = dimensions[0] // cards_per_row
         self.card_spacing_y = dimensions[1] // cards_per_column
         self.card_size_x = self.card_spacing_x - space_between
         self.card_size_y = self.card_spacing_y - space_between
+
+        # create one card for each space on a grid by going through all possible x, y grid coordinate combinations
         self.deck = []
         self.screen = screen
-        for x in range(cards_per_row):
+        card_dimension = [self.card_size_x, self.card_size_y]
+        for x in range(cards_per_row - 1):  # the - 1 ensures cards are not created for the column reserved for score
             for y in range(cards_per_column):
-                card_dimension = [self.card_size_x, self.card_size_y]
+
+                # find the location of the card by multiplying the card spacing by the grid coordinate value for
+                # the respective axis by it's card spacing, then adding the space_between to prevent cards touching
                 card_location = [space_between + self.card_spacing_x * x, space_between + self.card_spacing_y * y]
+
+                # create the card for that spot on the grid before continuing to the next grid coordinate
                 self.deck.append(Card(card_dimension, card_location, None, screen))
 
     def draw(self):
